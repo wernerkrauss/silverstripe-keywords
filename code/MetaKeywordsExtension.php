ba29bc854d8759b4696f1e5aa38d64fd1bb45058
Werner M. Krauß <werner.krauss@netwerkstatt.at>
2018-10-08 12:02:43 +0200

    Update to SS4

--- code/MetaKeywordsExtension.php
+++ code/MetaKeywordsExtension.php
@@ -1,28 +1,44 @@
 <?php
 
-class MetaKeywordsExtension extends DataExtension {
+namespace ArnoPoot\MetaKeywords;
 
-	private static $db = array(
-		"MetaKeywords" => "Varchar(1024)",
-	);
 
-	public function updateCMSFields(FieldList $fields) {
-		$metaData = $fields->fieldByName('Root.Main.Metadata');
+use SilverStripe\CMS\Model\SiteTree;
+use SilverStripe\Core\Convert;
+use SilverStripe\Forms\CompositeField;
+use SilverStripe\Forms\FieldList;
+use SilverStripe\Forms\TextareaField;
+use SilverStripe\ORM\DataExtension;
 
-		$metaFieldTitle = new TextareaField("MetaKeywords", $this->owner->fieldLabel('MetaKeywords'));
-		$metaData->insertAfter($metaFieldTitle, 'MetaDescription');
 
-		return $fields;
-	}
+class MetaKeywordsExtension extends DataExtension
+{
 
-	public function updateFieldLabels(&$labels) {
-		$labels['MetaKeywords'] = _t('SiteTree.METAKEYWORDS', "Meta Keywords");
-	}
+    private static $db = [
+        'MetaKeywords' => 'Varchar(1024)',
+    ];
 
-	public function MetaTags(&$tags) {
-		if($this->owner->MetaKeywords) {
-			$tags .= "<meta name=\"keywords\" content=\"" . Convert::raw2att($this->owner->MetaKeywords) . "\" />\n";
-		}
-	}
+    public function updateCMSFields(FieldList $fields)
+    {
+        /** @var CompositeField $metaData */
+        $metaData = $fields->fieldByName('Root.Main.Metadata');
+
+        $metaFieldTitle = new TextareaField('MetaKeywords', $this->owner->fieldLabel('MetaKeywords'));
+        $metaData->insertAfter('MetaDescription', $metaFieldTitle);
+
+        return $fields;
+    }
+
+    public function updateFieldLabels(&$labels)
+    {
+        $labels['MetaKeywords'] = _t(SiteTree::class . 'METAKEYWORDS', 'Meta Keywords');
+    }
+
+    public function MetaTags(&$tags)
+    {
+        if ($this->owner->MetaKeywords) {
+            $tags .= '<meta name="keywords" content="' . Convert::raw2att($this->owner->MetaKeywords) . "\" />\n";
+        }
+    }
 
 }
